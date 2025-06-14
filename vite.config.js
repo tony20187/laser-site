61ae319d594e239140c0212dbf78397a6e162c80
tony20187 <tony20187@gmail.com>
2025-06-14 23:55:53 +0800

    push source code only

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/laser-site/',  // 這裡改成你的 repo 名稱，最後一定要有斜線 /
+  base: '/laser-site/',   // 你的github repo 名稱
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
   }
 });
